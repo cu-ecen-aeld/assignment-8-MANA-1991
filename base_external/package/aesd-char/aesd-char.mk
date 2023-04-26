247dd5ec8d2846487693a17a65d5350d49420a68
ifuterman <ifutermang@gmail.com>
2023-04-27 02:32:10 +0300

    socket server update

--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_VERSION = '72d35c3e48ed7fd9ed1522e09ed484ff93d2e3c1'
+AESD_CHAR_VERSION = '9c5247ad421aa1ab7196b46699ccfd558d5e636b'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
